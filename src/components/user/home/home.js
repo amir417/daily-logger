e565516c6e599786ee3250a4dfeea185186da10b
Amirreza Aazam <aazam.amirreza@gmail.com>
2022-12-31 09:21:32 -0500

    quick fix on home page

--- src/components/user/home/home.js
+++ src/components/user/home/home.js
@@ -8,14 +8,18 @@ function Home() {
 
   const navigate = useNavigate();
   function HandleClick() {
+    navigate ("/user/prevlogs");
+  }
+  function HandleClick2() {
     navigate ("/user/logs");
   }
 
   return (
-   
-    <button onClick={HandleClick} className='btn logger'> Click here to view your logs</button>
-    
-    );
+   <>
+   <button onClick={HandleClick} className='btn logger'> Click here to view your logs</button> 
+    <button onClick={HandleClick2} className='btn logger'> Click here to create a new log</button>
+    </>
+     );
 }
 
 export default Home;
